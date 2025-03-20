b313d31ee706504d09a489aa666fc9bccc76c202
Md. Sajid Anam Ifti <info@sajidifti.com>
2025-03-21 01:58:43 +0600

    Fixed

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -34,6 +34,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    phone: string;
     avatar?: string;
     email_verified_at: string | null;
     created_at: string;
